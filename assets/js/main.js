9227cbca548c33712987fc1fd4849f0d5196a1d6
DiegoNasc <diegod_dsn@hotmail.com>
2022-11-09 09:02:46 -0300

    Manipulação multiplas requisições

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
+}
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -8,11 +11,10 @@ function convertPokemonToLi(pokemon) {
 
             <div class="details">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join(' ')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
             </div>               
         </li>`    
 }
